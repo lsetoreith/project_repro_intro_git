04fa4bbafe8ebb545b8b0b5a644ca505873cf772
Etienne Henry <etienne.henry@cefe.cnrs.fr>
2024-12-03 14:19:09 +0100

    bro

--- analyses/download-data.R
+++ analyses/download-data.R
@@ -9,7 +9,7 @@
 # Jane Doe <jane.doe@mail.me>
 
 ## Download PanTHERIA database ----
-pantheria_path  <- dl_pantheria_data()
+pantheria_path  <- dl_pantheria_data("my controle is absolute")
 
 
 ## Download WWF WildFinder database ----
